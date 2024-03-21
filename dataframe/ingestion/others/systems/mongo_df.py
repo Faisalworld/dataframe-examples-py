ce5d0a23bac40f61c91e92f097acfcec11e6bea0
faisal <ftokdiya@gmail.com>
2024-03-21 16:11:47 +0530

    commit on 18/03/2024 16:20

--- dataframe/ingestion/others/systems/mongo_df.py
+++ dataframe/ingestion/others/systems/mongo_df.py
@@ -2,10 +2,11 @@ from pyspark.sql import SparkSession
 import yaml
 import os.path
 
-if __name__ == '__main__':
+from pyspark.sql.functions import col
 
+if __name__ == '__main__':
     os.environ["PYSPARK_SUBMIT_ARGS"] = (
-         '--packages "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" pyspark-shell'
+        '--packages "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" pyspark-shell'
     )
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app_config_path = os.path.abspath(current_dir + "/../../../../" + "application.yml")
@@ -20,19 +21,24 @@ if __name__ == '__main__':
     spark = SparkSession \
         .builder \
         .appName("Read ingestion enterprise applications") \
-        .config("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"])\
+        .config("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"]) \
         .getOrCreate()
     spark.sparkContext.setLogLevel('ERROR')
 
-    students = spark\
-        .read\
-        .format("com.mongodb.spark.sql.DefaultSource")\
-        .option("spark.mongodb.input.database", "addr")\
-        .option("spark.mongodb.input.collection", "address")\
-        .option("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"])\
+    address = spark \
+        .read \
+        .format("com.mongodb.spark.sql.DefaultSource") \
+        .option("spark.mongodb.input.database", "addr") \
+        .option("spark.mongodb.input.collection", "address") \
+        .option("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"]) \
         .load()
 
-    students.show()
+    address = address.withColumn("street", col("address.street")) \
+        .withColumn("city", col("address.city")) \
+        .withColumn("state", col("address.state")) \
+        .drop("address", "_id")
+
+    address.show()
 
 # spark-submit --master yarn --packages "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" dataframe/ingestion/others/systems/mongo_df.py
 # spark-submit --packages "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" MonksProject/com/test/source_data_loading.py
