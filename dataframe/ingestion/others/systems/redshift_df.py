3b26421b33a05ff371285b1466fe1ac6992621b0
faisal <ftokdiya@gmail.com>
2024-03-24 17:20:06 +0530

    commit on 18/03/2024 16:20

--- dataframe/ingestion/others/systems/redshift_df.py
+++ dataframe/ingestion/others/systems/redshift_df.py
@@ -73,6 +73,7 @@ if __name__ == '__main__':
         .option("url", url) \
         .option("dbtable", "public.txn_fct_new") \
         .option("tempdir", "s3://spark-faisal-spark/temp") \
+        .option("forward_spark_s3_credentials", "true") \
         .mode("overwrite") \
         .save()
 
